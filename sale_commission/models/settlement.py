41f1724ce251ce2144c46131c81f47b35f4ee890
ribrax <joaobraz@ribrax.com.br>
2021-07-30 15:54:58 -0300

    partner_id

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -176,8 +176,8 @@ class SettlementLine(models.Model):
     invoice = fields.Many2one(
         comodel_name='account.invoice', store=True, string="Invoice",
         related='invoice_line.invoice_id')
-    partner = fields.Many2one('partner_id', string="Partner",
-        related='invoice_line.partner_id'))    
+    partner_id = fields.Many2one('partner_id', string="Partner",
+        related='account_invoice_report.partner_id'))    
     agent = fields.Many2one(
         comodel_name="res.partner", readonly=True, related="agent_line.agent",
         store=True)
