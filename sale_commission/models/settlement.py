2a88cd30f5d96d5c9511f217ecd7dbd26eb250be
ribrax <joaobraz@ribrax.com.br>
2021-08-05 08:48:54 -0300

    .

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -177,7 +177,7 @@ class SettlementLine(models.Model):
         comodel_name='account.invoice', store=True, string="Invoice",
         related='invoice_line.invoice_id')
     origin = fields.Many2one(
-        comodel_name="account.invoice", string="Origin", store=True,
+        comodel_name="account.invoice.line", string="Origin", store=True,
         related='invoice_line.origin_id')  
     agent = fields.Many2one(
         comodel_name="res.partner", readonly=True, related="agent_line.agent",
