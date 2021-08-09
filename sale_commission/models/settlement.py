71d4ccc6006bb1336faff8af83c78d6ad97a5375
ribrax <joaobraz@ribrax.com.br>
2021-08-09 20:49:05 -0300

    .

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -196,7 +196,7 @@ class SettlementLine(models.Model):
         for record in self:
             record.commission_total = 0.0
             for line in record.agent_line:
-                record.commission_total += sum(x.amount for x in line.agent_line)
+                record.commission_total += sum(x.amount for x in line)
 
     commission_total = fields.Float(
         string="Commissions",
