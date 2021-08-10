eeac7baee8dbe97aba847a11c98de564798d78a7
ribrax <joaobraz@ribrax.com.br>
2021-08-10 16:27:48 -0300

    .

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -183,6 +183,7 @@ class SettlementLine(models.Model):
         store=True)
     settled_amount = fields.Monetary(
         related="agent_line.amount", readonly=True, store=True)
+        
     currency_id = fields.Many2one(
         related="agent_line.currency_id",
         store=True,
