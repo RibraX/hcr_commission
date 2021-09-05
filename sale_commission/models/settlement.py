3037badaadb65ce526bc37ed179c3ed4bab0773c
ribrax <joaobraz@ribrax.com.br>
2021-09-05 10:39:01 -0300

    .

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -263,7 +263,7 @@ class SettlementReport(models.Model):
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
 
-   @api.multi
+    @api.multi
     def _get_report_values(self, docids, data=None):
         docs = self.env['sale.commission.settlement.line'].browse(docids)
         return {
