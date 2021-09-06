7852092976780a86ffc4c2492fa1cdbb28e163a3
ribrax <joaobraz@ribrax.com.br>
2021-09-06 17:45:35 -0300

    adsdasda

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -167,9 +167,9 @@ class SettlementLine(models.Model):
         relation='settlement_agent_line_rel', column1='settlement_id',
         column2='agent_line_id', required=True)
     date = fields.Date(related="agent_line.invoice_date", store=True)
-    # invoice_line = fields.Many2one(
-    #     comodel_name='account.invoice.line', store=True,
-    #     related='agent_line.object_id')
+    invoice_line = fields.Many2one(
+        comodel_name='account.invoice.line', store=True,
+        related='agent_line.object_id')
     invoice = fields.Many2one(
         comodel_name='account.invoice', store=True, string="Invoice",
         related='invoice_line.invoice_id')
