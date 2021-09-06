19bc2c56ed979e37ab6823f1cbe9de2444004ec2
ribrax <joaobraz@ribrax.com.br>
2021-09-06 16:29:26 -0300

    saaSas

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -233,52 +233,65 @@ class ComissaoTeste(models.Model):
     _name = "sale.commission.teste"
     _description = "Sale Commission Analysis Report"
     _auto = False
-    _rec_name = 'a_commission_id'
+    _rec_name = 'commission_id'
 
+    
 
     @api.model
     def _get_selection_invoice_state(self):
         return self.env['account.invoice'].fields_get(
             allfields=['state'])['state']['selection']
 
-    # invoice_state = fields.Selection(selection='_get_selection_invoice_state',
-    #                                  string='Invoice Status', readonly=True)
-    # so_origin = fields.Date('Date Invoice', readonly=True)
-    # so_origin = fields.Many2one('account_invoice.origin', 'SO', readonly=True)
-    # commission_total = fields.Many2one('account_invoice.commission_total', 'Comissão', readonly=True)
-    # partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
-    # agent_id = fields.Many2one('res.partner', 'Agent', readonly=True)
-    # categ_id = fields.Many2one(
-    #     'product.category',
-    #     'Category of Product',
-    #     readonly=True)
-    # # product_id = fields.Many2one('product.product', 'Product', readonly=True)
-    # uom_id = fields.Many2one('uom.uom', 'Unit of Measure', readonly=True)
-    # quantity = fields.Float('# of Qty', readonly=True)
-    # price_unit = fields.Float('Price unit', readonly=True)
-    # price_subtotal = fields.Float('Price subtotal', readonly=True)
-    # price_subtotal_signed = fields.Float(
-    #     string='Price subtotal signed',
-    #     readonly=True,
-    # )
-    # percentage = fields.Integer('Percentage of commission', readonly=True)
-    # amount = fields.Float('Amount', readonly=True)
-    # invoice_line_id = fields.Many2one(
-    #     'account.invoice.line',
-    #     'Invoice line',
-    #     readonly=True)
-    # settled = fields.Boolean('Settled', readonly=True)
-    # commission_id = fields.Many2one(
-    #     'sale.commission',
-    #     'Sale commission',
-    #     readonly=True)
+    invoice_state = fields.Selection(selection='_get_selection_invoice_state',
+                                     string='Invoice Status', readonly=True)
+    date_invoice = fields.Date('Date Invoice', readonly=True)
+    company_id = fields.Many2one('res.company', 'Company', readonly=True)
+    partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
+    agent_id = fields.Many2one('res.partner', 'Agent', readonly=True)
+    categ_id = fields.Many2one(
+        'product.category',
+        'Category of Product',
+        readonly=True)
+    # product_id = fields.Many2one('product.product', 'Product', readonly=True)
+    uom_id = fields.Many2one('uom.uom', 'Unit of Measure', readonly=True)
+    quantity = fields.Float('# of Qty', readonly=True)
+    price_unit = fields.Float('Price unit', readonly=True)
+    price_subtotal = fields.Float('Price subtotal', readonly=True)
+    price_subtotal_signed = fields.Float(
+        string='Price subtotal signed',
+        readonly=True,
+    )
+    percentage = fields.Integer('Percentage of commission', readonly=True)
+    amount = fields.Float('Amount', readonly=True)
+    invoice_line_id = fields.Many2one(
+        'account.invoice.line',
+        'Invoice line',
+        readonly=True)
+    settled = fields.Boolean('Settled', readonly=True)
+    commission_id = fields.Many2one(
+        'sale.commission',
+        'Sale commission',
+        readonly=True)
 
     def _select(self):
         select_str = """
-            SELECT MIN(aila.id) AS id,            
-            ai.origin AS so_origin,
-            ai.commission_total AS commission_total,
-            SUM(aila.amount) AS amount
+            SELECT MIN(aila.id) AS id,
+            ai.partner_id AS partner_id,
+            ai.state AS invoice_state,
+            ai.date_invoice AS date_invoice,
+            ail.company_id AS company_id,
+            rp.id AS agent_id,
+            pt.categ_id AS categ_id,
+            pt.uom_id AS uom_id,
+            SUM(ail.quantity) AS quantity,
+            AVG(ail.price_unit) AS price_unit,
+            SUM(ail.price_subtotal) AS price_subtotal,
+            SUM(ail.price_subtotal_signed) AS price_subtotal_signed,
+            AVG(sc.fix_qty) AS percentage,
+            SUM(aila.amount) AS amount,
+            ail.id AS invoice_line_id,
+            aila.settled AS settled,
+            aila.commission AS commission_id
         """
         # ail.product_id AS product_id,
         return select_str
@@ -288,14 +301,25 @@ class ComissaoTeste(models.Model):
             account_invoice_line_agent aila
             LEFT JOIN account_invoice_line ail ON ail.id = aila.object_id
             INNER JOIN account_invoice ai ON ai.id = ail.invoice_id
+            LEFT JOIN sale_commission sc ON sc.id = aila.commission
+            LEFT JOIN product_product pp ON pp.id = ail.product_id
+            INNER JOIN product_template pt ON pp.product_tmpl_id = pt.id
+            LEFT JOIN res_partner rp ON aila.agent = rp.id
         """
         return from_str
 
     def _group_by(self):
         group_by_str = """
-            GROUP BY 
-            ai.origin,
-            ai.commission_total
+            GROUP BY ai.partner_id,
+            ai.state,
+            ai.date_invoice,
+            ail.company_id,
+            rp.id,
+            pt.categ_id,
+            pt.uom_id,
+            ail.id,
+            aila.settled,
+            aila.commission
         """
         # ail.product_id,
         return group_by_str
