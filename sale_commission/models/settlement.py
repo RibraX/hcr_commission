76e73e6898a1096dc6970fefac4e33470c0db040
ribrax <joaobraz@ribrax.com.br>
2021-09-06 20:28:44 -0300

    .

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -261,7 +261,7 @@ class SettlementLine(models.Model):
 
     @api.model_cr
     def init(self):
-        self._table = sale.comission.settlement.line
+        self._table = sale_comission.settlement.line
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
 
