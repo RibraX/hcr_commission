33bc4461801a2249c813b33612ab6ec83d50f5f1
ribrax <joaobraz@ribrax.com.br>
2021-09-06 20:22:56 -0300

    .

--- sale_commission/models/settlement.py
+++ sale_commission/models/settlement.py
@@ -265,172 +265,172 @@ class SettlementLine(models.Model):
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
 
-        
 
 
-class SettlementReport(models.Model):
-    # _inherit = "sale.commission.settlement.line"
-    _name = "sale.commission.settlement.report"
-    _description = "Sales Commission Settlement Report"
-    _auto = False
-    # _rec_name = 'date'
-    _order = 'origin'
 
-    @api.model
-    def _get_done_states(self):
-        return ['sale', 'done', 'paid']
+# class SettlementReport(models.Model):
+#     # _inherit = "sale.commission.settlement.line"
+#     _name = "sale.commission.settlement.report"
+#     _description = "Sales Commission Settlement Report"
+#     _auto = False
+#     # _rec_name = 'date'
+#     _order = 'origin'
+
+#     @api.model
+#     def _get_done_states(self):
+#         return ['sale', 'done', 'paid']
 
 
-    id = fields.Integer('ID Referencia', readonly=True)
-    settlement = fields.Integer('Fechamento', readonly=True)
-    agent = fields.Integer('Representante', readonly=True)
-    origin = fields.Char('Pedido', readonly=True)
-    # customer = fields.Char('Cliente', readonly=True)
-    comm_total = fields.Float('Comissão', readonly=True)
+#     id = fields.Integer('ID Referencia', readonly=True)
+#     settlement = fields.Integer('Fechamento', readonly=True)
+#     agent = fields.Integer('Representante', readonly=True)
+#     origin = fields.Char('Pedido', readonly=True)
+#     # customer = fields.Char('Cliente', readonly=True)
+#     comm_total = fields.Float('Comissão', readonly=True)
             
-    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        with_ = ("WITH %s" % with_clause) if with_clause else ""
+#     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+#         with_ = ("WITH %s" % with_clause) if with_clause else ""
 
-        select_ = """
-            settlement as id,
-            settlement,
-            agent,
-            origin,
-            comm_total
-        """
+#         select_ = """
+#             settlement as id,
+#             settlement,
+#             agent,
+#             origin,
+#             comm_total
+#         """
 
-        for field in fields.values():
-            select_ += field
+#         for field in fields.values():
+#             select_ += field
 
-        from_ = """
-                sale_commission_settlement_line
-                %s
-        """ % from_clause
+#         from_ = """
+#                 sale_commission_settlement_line
+#                 %s
+#         """ % from_clause
 
-        groupby_ = """
-            settlement,
-            agent,
-            origin,
-            comm_total
-             %s
-        """ % (groupby)
+#         groupby_ = """
+#             settlement,
+#             agent,
+#             origin,
+#             comm_total
+#              %s
+#         """ % (groupby)
 
-        return '%s (SELECT %s FROM %s  GROUP BY %s)' % (with_, select_, from_, groupby_)
+#         return '%s (SELECT %s FROM %s  GROUP BY %s)' % (with_, select_, from_, groupby_)
 
-    @api.model_cr
-    def init(self):
-        # self._table = sale_report
-        tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
+#     @api.model_cr
+#     def init(self):
+#         # self._table = sale_report
+#         tools.drop_view_if_exists(self.env.cr, self._table)
+#         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
 
-class ComissaoTeste(models.Model):
-    _inherit = "sale.commission.settlement.line"
-    _name = "sale.commission.teste"
-    _description = "Sale Commission Analysis Report"
-    _auto = False
-    #_rec_name = 'commission_id'
+# class ComissaoTeste(models.Model):
+#     _inherit = "sale.commission.settlement.line"
+#     _name = "sale.commission.teste"
+#     _description = "Sale Commission Analysis Report"
+#     _auto = False
+#     #_rec_name = 'commission_id'
 
     
 
-    @api.model
-    def _get_selection_invoice_state(self):
-        return self.env['account.invoice'].fields_get(
-            allfields=['state'])['state']['selection']
+#     @api.model
+#     def _get_selection_invoice_state(self):
+#         return self.env['account.invoice'].fields_get(
+#             allfields=['state'])['state']['selection']
 
-    invoice_state = fields.Selection(selection='_get_selection_invoice_state',
-                                     string='Invoice Status', readonly=True)
-    date_invoice = fields.Date('Date Invoice', readonly=True)
-    company_id = fields.Many2one('res.company', 'Company', readonly=True)
-    partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
-    agent_id = fields.Many2one('res.partner', 'Agent', readonly=True)
-    categ_id = fields.Many2one(
-        'product.category',
-        'Category of Product',
-        readonly=True)
-    # product_id = fields.Many2one('product.product', 'Product', readonly=True)
-    uom_id = fields.Many2one('uom.uom', 'Unit of Measure', readonly=True)
-    quantity = fields.Float('# of Qty', readonly=True)
-    price_unit = fields.Float('Price unit', readonly=True)
-    price_subtotal = fields.Float('Price subtotal', readonly=True)
-    price_subtotal_signed = fields.Float(
-        string='Price subtotal signed',
-        readonly=True,
-    )
-    percentage = fields.Integer('Percentage of commission', readonly=True)
-    amount = fields.Float('Amount', readonly=True)
-    invoice_line_id = fields.Many2one(
-        'account.invoice.line',
-        'Invoice line',
-        readonly=True)
-    settled = fields.Boolean('Settled', readonly=True)
-    commission_id = fields.Many2one(
-        'sale.commission',
-        'Sale commission',
-        readonly=True)
-    invoice_origin = fields.Many2one('account_invoice.origin', 'SO Origin', readonly=True)
+#     invoice_state = fields.Selection(selection='_get_selection_invoice_state',
+#                                      string='Invoice Status', readonly=True)
+#     date_invoice = fields.Date('Date Invoice', readonly=True)
+#     company_id = fields.Many2one('res.company', 'Company', readonly=True)
+#     partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
+#     agent_id = fields.Many2one('res.partner', 'Agent', readonly=True)
+#     categ_id = fields.Many2one(
+#         'product.category',
+#         'Category of Product',
+#         readonly=True)
+#     # product_id = fields.Many2one('product.product', 'Product', readonly=True)
+#     uom_id = fields.Many2one('uom.uom', 'Unit of Measure', readonly=True)
+#     quantity = fields.Float('# of Qty', readonly=True)
+#     price_unit = fields.Float('Price unit', readonly=True)
+#     price_subtotal = fields.Float('Price subtotal', readonly=True)
+#     price_subtotal_signed = fields.Float(
+#         string='Price subtotal signed',
+#         readonly=True,
+#     )
+#     percentage = fields.Integer('Percentage of commission', readonly=True)
+#     amount = fields.Float('Amount', readonly=True)
+#     invoice_line_id = fields.Many2one(
+#         'account.invoice.line',
+#         'Invoice line',
+#         readonly=True)
+#     settled = fields.Boolean('Settled', readonly=True)
+#     commission_id = fields.Many2one(
+#         'sale.commission',
+#         'Sale commission',
+#         readonly=True)
+#     invoice_origin = fields.Many2one('account_invoice.origin', 'SO Origin', readonly=True)
     
 
-    def _select(self):
-        select_str = """
-            SELECT MIN(aila.id) AS id,
-            ai.partner_id AS partner_id,
-            ai.state AS invoice_state,
-            ai.origin AS invoice_origin,
-            ai.date_invoice AS date_invoice,
-            ail.company_id AS company_id,
-            rp.id AS agent_id,
-            pt.categ_id AS categ_id,
-            pt.uom_id AS uom_id,
-            SUM(ail.quantity) AS quantity,
-            AVG(ail.price_unit) AS price_unit,
-            SUM(ail.price_subtotal) AS price_subtotal,
-            SUM(ail.price_subtotal_signed) AS price_subtotal_signed,
-            AVG(sc.fix_qty) AS percentage,
-            SUM(aila.amount) AS amount,
-            ail.id AS invoice_line_id,
-            aila.settled AS settled,
-            aila.commission AS commission_id
-        """
-        # ail.product_id AS product_id,
-        return select_str
+#     def _select(self):
+#         select_str = """
+#             SELECT MIN(aila.id) AS id,
+#             ai.partner_id AS partner_id,
+#             ai.state AS invoice_state,
+#             ai.origin AS invoice_origin,
+#             ai.date_invoice AS date_invoice,
+#             ail.company_id AS company_id,
+#             rp.id AS agent_id,
+#             pt.categ_id AS categ_id,
+#             pt.uom_id AS uom_id,
+#             SUM(ail.quantity) AS quantity,
+#             AVG(ail.price_unit) AS price_unit,
+#             SUM(ail.price_subtotal) AS price_subtotal,
+#             SUM(ail.price_subtotal_signed) AS price_subtotal_signed,
+#             AVG(sc.fix_qty) AS percentage,
+#             SUM(aila.amount) AS amount,
+#             ail.id AS invoice_line_id,
+#             aila.settled AS settled,
+#             aila.commission AS commission_id
+#         """
+#         # ail.product_id AS product_id,
+#         return select_str
 
-    def _from(self):
-        from_str = """
-            account_invoice_line_agent aila
-            LEFT JOIN account_invoice_line ail ON ail.id = aila.object_id
-            INNER JOIN account_invoice ai ON ai.id = ail.invoice_id
-            LEFT JOIN sale_commission sc ON sc.id = aila.commission
-            LEFT JOIN product_product pp ON pp.id = ail.product_id
-            INNER JOIN product_template pt ON pp.product_tmpl_id = pt.id
-            LEFT JOIN res_partner rp ON aila.agent = rp.id
-        """
-        return from_str
+#     def _from(self):
+#         from_str = """
+#             account_invoice_line_agent aila
+#             LEFT JOIN account_invoice_line ail ON ail.id = aila.object_id
+#             INNER JOIN account_invoice ai ON ai.id = ail.invoice_id
+#             LEFT JOIN sale_commission sc ON sc.id = aila.commission
+#             LEFT JOIN product_product pp ON pp.id = ail.product_id
+#             INNER JOIN product_template pt ON pp.product_tmpl_id = pt.id
+#             LEFT JOIN res_partner rp ON aila.agent = rp.id
+#         """
+#         return from_str
 
-    def _group_by(self):
-        group_by_str = """
-            GROUP BY ai.partner_id,
-            ai.state,
-            ai.origin,
-            ai.date_invoice,
-            ail.company_id,
-            rp.id,
-            pt.categ_id,
-            pt.uom_id,
-            ail.id,
-            aila.settled,
-            aila.commission
-        """
-        # ail.product_id,
-        return group_by_str
+#     def _group_by(self):
+#         group_by_str = """
+#             GROUP BY ai.partner_id,
+#             ai.state,
+#             ai.origin,
+#             ai.date_invoice,
+#             ail.company_id,
+#             rp.id,
+#             pt.categ_id,
+#             pt.uom_id,
+#             ail.id,
+#             aila.settled,
+#             aila.commission
+#         """
+#         # ail.product_id,
+#         return group_by_str
 
-    @api.model
-    def init(self):
-        tools.drop_view_if_exists(self._cr, self._table)
-        self._cr.execute(
-            "CREATE or REPLACE VIEW %s AS ( %s FROM ( %s ) %s )", (
-                AsIs(self._table),
-                AsIs(self._select()),
-                AsIs(self._from()),
-                AsIs(self._group_by())
-            ),
-        )
+#     @api.model
+#     def init(self):
+#         tools.drop_view_if_exists(self._cr, self._table)
+#         self._cr.execute(
+#             "CREATE or REPLACE VIEW %s AS ( %s FROM ( %s ) %s )", (
+#                 AsIs(self._table),
+#                 AsIs(self._select()),
+#                 AsIs(self._from()),
+#                 AsIs(self._group_by())
+#             ),
+#         )
